8476f8353965131d82fbc045820a81597428789f
jahongir7 <jahongir95119@gmail.com>
2023-05-21 18:24:25 +0500

    login register

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,23 +1,62 @@
-import { Link } from "react-router-dom";
 import classes from "../assets/styles/login.module.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 const LoginPage = () => {
   AOS.init({
     duration: 300,
   });
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [myData, setMyData] = useState({});
+  const navigate = useNavigate();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await fetch("http://localhost:1337/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => setMyData(data))
+      .catch((err) => console.log(err));
+  }
+  console.log(myData);
+  useEffect(() => {
+    if (myData.user) {
+      localStorage.setItem("token", myData.user);
+      alert("Login successful");
+      navigate("/ask");
+    }
+  }, [myData]);
+
   return (
     <div className={classes.login_page}>
       <form onSubmit={handleSubmit} data-aos="zoom-in">
         <h2>Kirish</h2>
-        <input type="text" placeholder="Foydalanuvchi ismi" />
-        <input type="password" placeholder="Parol" />
-        <Link to="/ask">
-          <button type="submit">Kirish</button>
-        </Link>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+        />
+
+        <button type="submit">Kirish</button>
       </form>
     </div>
   );
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,23 +1,69 @@
-import { Link } from "react-router-dom";
 import classes from "../assets/styles/login.module.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 const RegisterPage = () => {
   AOS.init({
     duration: 300,
   });
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [myData, setMyData] = useState({});
+  const navigate = useNavigate();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await fetch("http://localhost:1337/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => setMyData(data))
+      .catch((err) => console.log(err));
+  }
+  console.log(myData);
+  useEffect(() => {
+    if (myData.status === "ok") {
+      navigate("/login");
+    }
+  }, [myData]);
+
   return (
     <div className={classes.login_page}>
       <form onSubmit={handleSubmit} data-aos="zoom-in">
-        <h2>Ro'yhatdan o'tish</h2>
-        <input type="text" placeholder="Foydalanuvchi ismi" />
-        <input type="password" placeholder="Parol" />
-        <Link to="/login">
-          <button type="submit">Ro'yhatdan o'tish</button>
-        </Link>
+        <h2>Register</h2>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Ism"
+          name="name"
+        />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+        />
+
+        <button type="submit">Kirish</button>
       </form>
     </div>
   );
